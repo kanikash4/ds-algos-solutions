7e5ad0774d9b877f95d3a4a4a9de1fbf88e83c14
Kanika Sharma <kanikash4@gmail.com>
2017-11-05 23:47:50 +0530

    nQueen Problem

---
+++ DynamicProgramming/001_nQueenProblem/index.js
@@ -0,0 +1,55 @@
+/**
+ * n-queen problem
+ * print_board(place_next_queen(4, 4))  will give output after console : console.log('\niterations: ', iterations) as
+ *  # Q # # 
+ * # # # Q 
+ * Q # # # 
+ * # # Q # 
+ * 
+ */
+
+'use strict';
+
+var iterations = 0;
+
+var print_board = function (columns) {
+  var n = columns.length, row = 0, col = 0;
+  while (row < n) {
+    while (col < n) {
+      process.stdout.write(columns[row] === col ? 'Q ' : '# ');
+      col++;
+    }
+    process.stdout.write('\n');
+    col = 0;
+    row++;
+  }
+};
+
+var has_conflict = function (columns) {
+  var len = columns.length, last = columns[len - 1], previous = len - 2;
+
+  while (previous >= 0) {
+    if (columns[previous] === last) return true;
+    if (last - (len - 1) === columns[previous] - previous) return true;
+    if (last + (len - 1) === columns[previous] + previous) return true;
+    previous--;
+  }
+  return false;
+};
+
+var place_next_queen = function (total, queens, columns) {
+  if (queens === 0) return columns;
+  columns = columns || [];
+
+  for (var column = 0; column < total; column++) {
+    columns.push(column);
+    iterations++;
+    if (!has_conflict(columns) &&
+        place_next_queen(total, queens - 1, columns)) {
+      return columns;
+    }
+    columns.pop(column);
+  }
+
+  return null;
+};
