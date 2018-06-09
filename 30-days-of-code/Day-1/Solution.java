6c552d056777f30e27a5371280e3727f30feb108
Kanika Sharma <kanikash4@gmail.com>
2018-06-09 22:13:18 +0530

    File rename

--- 30-days-of-code/Day-1/Solution.java
+++ 30-days-of-code/Day-1/Solution.java
@@ -1,15 +1,38 @@
 /**
- * Input Format:
- * A single line of text denoting  (the variable whose contents must be printed).
+ * Use the  operator to perform the following operations: 
+ * Print the sum of  plus your int variable on a new line.
+ * Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+ * Concatenate  with the string you read as input and print the result on a new line.
  *
- * Output Format:
- * Print Hello, World. on the first line, and the contents of  on the second line.
+ * Input Format
  *
- * Input: Welcome to 30 Days of Code!
- * Expected Output: 
- * Hello, World.
- * Welcome to 30 Days of Code!
+ * The first line contains an integer that you must sum with . 
+ * The second line contains a double that you must sum with . 
+ * The third line contains a string that you must concatenate with .
+ *
+ * Output Format
+ * 
+ * Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+ *
+ * Sample Input: 
+ *
+ * 12
+ * 4.0
+ * is the best place to learn and practice coding! 
+ * 
+ * Sample Output: 
+ * 
+ * 16
+ * 8.0
+ * HackerRank is the best place to learn and practice coding!
+ * 
+ * Explanation:  
+ * When we sum the integers 4 and 12, we get the integer 16.
+ * When we sum the floating-point numbers 4.0 and 4.0, we get 8.0. 
+ * When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!. 
+ *
  */
+
 
 import java.io.*;
 import java.util.*;
@@ -18,21 +41,33 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-	public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
-		Scanner scan = new Scanner(System.in); 
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
 		
-		// Read a full line of input from stdin and save it to our variable, inputString.
-		String inputString = scan.nextLine(); 
+        Scanner scan = new Scanner(System.in);
 
-		// Close the scanner object, because we've finished reading 
-        // all of the input from stdin needed for this challenge.
-		scan.close(); 
-      
-		// Print a string literal saying "Hello, World." to stdout.
-		System.out.println("Hello, World.");
-      
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.println(inputString);
-	}
+        /* Declare second integer, double, and String variables. */
+        int ii;
+        double dd;
+        String ss = "";
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        ii = scan.nextInt();
+        dd = scan.nextDouble();
+        scan.nextLine(); // to get rid of newline at end of line 2 of input
+        ss = scan.nextLine(); 
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + ii);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d + dd);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+ss);
+       scan.close();
+    }
 }
