ea435afee2908804164760745b2b21bc6be62e78
Kanika Sharma <kanikash4@gmail.com>
2018-06-15 22:32:19 +0530

    Day-08 Hackerrank Map/Dictionary problem solution

---
+++ 30-days-of-code/Day-8/Solution.java
@@ -0,0 +1,65 @@
+/*
+
+Objective 
+Today, we're learning about Key-Value pair mappings using a Map or Dictionary data structure. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
+You will then be given an unknown number of names to query your phone book for. For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; if an entry for name is not found, print Not found instead.
+
+Note: Your phone book should be a Dictionary/Map/HashMap data structure.
+
+Input Format
+
+The first line contains an integer, n, denoting the number of entries in the phone book. 
+Each of the n subsequent lines describes an entry in the form of 2 space-separated values on a single line. The first value is a friend's name, and the second value is an 8-digit phone number.
+
+After the n lines of phone book entries, there are an unknown number of lines of queries. Each line (query) contains a name to look up, and you must continue reading lines until there is no more input.
+
+Note: Names consist of lowercase English alphabetic letters and are first names only.
+
+Sample Input
+
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+Sample Output
+
+sam=99912222
+Not found
+harry=12299933
+
+
+*/
+
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            map.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if (map.containsKey(s)) {
+                System.out.println(s + "=" + map.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
