fcaa8c5aacb583a280075e04e8cd5dfe41f65321
Kanika Sharma <kanikash4@gmail.com>
2018-06-18 18:37:30 +0530

    Day 11 2d array problem solution

---
+++ 30-days-of-code/Day-11/Solution.java
@@ -0,0 +1,122 @@
+/*
+
+2-D Array::
+
+Objective 
+Today, we're building on our knowledge of Arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video!
+
+Context 
+Given a 6 x 6 2D Array, A:
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+Task 
+Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+Input Format
+
+There are 6 lines of input, where each line contains 6 space-separated integers describing 2D Array A; every value in A will be in the inclusive range of -9 to 9.
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in A.
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+        System.out.println(maxHourglass(arr));
+    }
+     
+    private static int findSum(int [][] arr, int r, int c) {
+        int sum = arr[r+0][c+0] + arr[r+0][c+1] + arr[r+0][c+2] + arr[r+1][c+1] + 
+                  arr[r+2][c+0] + arr[r+2][c+1] + arr[r+2][c+2];
+       
+        return sum;
+    }
+
+    public static int maxHourglass(int [][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                int sum = findSum(arr, row, col);
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+    
+
+    
+}
