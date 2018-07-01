de6c618721649f98a099b1ef6f9e19b1c364b13e
Kanika Sharma <kanikash4@gmail.com>
2018-07-02 00:54:04 +0530

    Reverse user i/p string with recursion

---
+++ java/Strings/ReverseWordsInString/Reverse_String_With_User_Input/Solution.java
@@ -0,0 +1,26 @@
+/*
+  Reverse the User entered String with recursion 
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String str;
+        System.out.println("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+        scanner.close();
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
